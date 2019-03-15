fe13841c0cd5b5642e07a7bf6034657511a5d14b
Nate <nategreat13@hotmail.com>
2019-03-15 11:07:20 -0700

    Update version

--- l2.py
+++ l2.py
@@ -2,10 +2,10 @@ from ryu.base import app_manager
 from ryu.controller import ofp_event
 from ryu.controller.handler import MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
-from ryu.ofproto import ofproto_v1_0
+from ryu.ofproto import ofproto_v1_3
 
 class L2Switch(app_manager.RyuApp):
-    OFP_VERSIONS = [ofproto_v1_0.OFP_VERSION]
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
     
     def __init__(self, *args, **kwargs):
         super(L2Switch, self).__init__(*args, **kwargs)
