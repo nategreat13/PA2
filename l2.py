1a283f44912f1c3e1f23ac7afa3d68bba4d147c9
Nate <nategreat13@hotmail.com>
2019-03-16 10:09:44 -0700

    New commit

--- l2.py
+++ l2.py
@@ -18,7 +18,15 @@ class L2Switch(app_manager.RyuApp):
         ofp_parser = dp.ofproto_parser
         in_port = msg.match['in_port']
         
-        print("In Port: %s", in_port)
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocols(ethernet.ethernet)[0]
+        
+        dst = eth.dst
+        src = eth.src
+        
+        dpid = datapath.id
+        
+        self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
         
         actions = [ofp_parser.OFPActionOutput(ofp.OFPP_FLOOD)]
         out = ofp_parser.OFPPacketOut(datapath=dp, buffer_id=msg.buffer_id, in_port=in_port, actions=actions)
