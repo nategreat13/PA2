24c690a4f8f718751d849504e0473df4265c6d24
Nate <nategreat13@hotmail.com>
2019-03-19 17:59:33 -0600

    Added src_ip and dst_ip

--- monitor.py
+++ monitor.py
@@ -5,6 +5,7 @@ from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_3
 from ryu.lib.packet import packet
 from ryu.lib.packet import ethernet
+from ryu.lib.packet import arp
 from ryu.lib.packet import ether_types
 
 class monitor(app_manager.RyuApp):
@@ -19,8 +20,13 @@ class monitor(app_manager.RyuApp):
         msg = ev.msg
         pkt = packet.Packet(msg.data)
         eth = pkt.get_protocols(ethernet.ethernet)[0]
+        arp = pkt.get_protocols(arp.arp)[0]
         
         dst = eth.dst
         src = eth.src
         
+        src_ip = arp.src_ip
+        dst_ip = arp.dst_ip
+        
         self.logger.info("packet in %s %s", src, dst)
+        self.logger.info("packet in %s %s", src_ip, dst_ip)
