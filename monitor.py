90b681ebc43b5843ce5a738e07cd2b74a4eb883a
Nate <nategreat13@hotmail.com>
2019-03-19 18:26:01 -0600

    newnew

--- monitor.py
+++ monitor.py
@@ -37,7 +37,7 @@ class monitor(app_manager.RyuApp):
         port = msg.match['in_port']
 
         self.logger.info("----------------------")
-#        self.logger.info("Packet ( %s) Received on Port(%s) Eth %s", packet_count, port, "ARP")
+        self.logger.info("Packet ( %s) Received on Port(%s) Eth %s", packet_count, port, "ARP")
 #        self.logger.info("\tARP")
 #        self.logger.info("\t\tSrc  IP: %s", src_ip)
 #        self.logger.info("\t\tDest IP: %s", dst_ip)
