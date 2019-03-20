0d934ba664f37521f4dffa0724941a1ad3f5294a
Nate <nategreat13@hotmail.com>
2019-03-19 18:04:09 -0600

    Test

--- monitor.py
+++ monitor.py
@@ -16,17 +16,28 @@ class monitor(app_manager.RyuApp):
     
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def packet_in_handler(self, ev):
-        
         msg = ev.msg
-        pkt = packet.Packet(msg.data)
-        eth = pkt.get_protocol(ethernet.ethernet)
-        arp = pkt.get_protocol(arp.arp)
-        
-        dst = eth.dst
-        src = eth.src
-        
-        src_ip = arp.src_ip
-        dst_ip = arp.dst_ip
-        
-        self.logger.info("packet in %s %s", src, dst)
-        self.logger.info("packet in %s %s", src_ip, dst_ip)
+        datapath = msg.datapath
+        port = msg.match['in_port']
+        pkt = packet.Packet(data=msg.data)
+        self.logger.info("packet-in %s" % (pkt,))
+        pkt_ethernet = pkt.get_protocol(ethernet.ethernet)
+        if not pkt_ethernet:
+            return
+        pkt_arp = pkt.get_protocol(arp.arp)
+
+
+#
+#        msg = ev.msg
+#        pkt = packet.Packet(msg.data)
+#        eth = pkt.get_protocol(ethernet.ethernet)
+#        arp = pkt.get_protocol(arp.arp)
+#
+#        dst = eth.dst
+#        src = eth.src
+#
+#        src_ip = arp.src_ip
+#        dst_ip = arp.dst_ip
+#
+#        self.logger.info("packet in %s %s", src, dst)
+#        self.logger.info("packet in %s %s", src_ip, dst_ip)
