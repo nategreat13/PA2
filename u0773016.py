40f02f1ab5868dc104f00a2245c3bcca9ff6ed99
Nate <nategreat13@hotmail.com>
2019-03-26 22:13:49 -0600

    s

--- u0773016.py
+++ u0773016.py
@@ -16,7 +16,7 @@ from ryu.lib.packet import arp
 from ryu.lib.packet import ipv4
 from ryu.lib.packet import icmp
 from ryu.lib.packet import ether_types
-#from ryu import cfg
+from ryu import cfg
 
 '''
     A simple controller that intercepts ARP and PING
@@ -27,15 +27,15 @@ class monitor(app_manager.RyuApp):
     
     def __init__(self, *args, **kwargs):
         super(monitor, self).__init__(*args, **kwargs)
-#        CONF = cfg.CONF
-#        CONF.register_opts([
-#            cfg.IntOpt('front_end_testers', default=0, help = ('Number of Front End Machines')),
-#            cfg.IntOpt('back_end_testers', default=0, help = ('Number of Back End Machines')),
-#            cfg.StrOpt('virtual_ip', default='default', help = ('Virtual IP'))])
+        CONF = cfg.CONF
+        CONF.register_opts([
+            cfg.IntOpt('front_end_testers', default=0, help = ('Number of Front End Machines')),
+            cfg.IntOpt('back_end_testers', default=0, help = ('Number of Back End Machines')),
+            cfg.StrOpt('virtual_ip', default='default', help = ('Virtual IP'))])
 
-#        print 'front_end_testers = {}'.format(CONF.front_end_testers))
-#        print 'back_end_testers = {}'.format(CONF.back_end_testers))
-#        print 'virtual_ip = {}'.format(CONF.virtual_ip))
+        print 'front_end_testers = {}'.format(CONF.front_end_testers))
+        print 'back_end_testers = {}'.format(CONF.back_end_testers))
+        print 'virtual_ip = {}'.format(CONF.virtual_ip))
         self.packet_count = 1 # Counter for the packet number
         
         self.h5count = 0
@@ -56,13 +56,6 @@ class monitor(app_manager.RyuApp):
         pkt_arp = pkt.get_protocol(arp.arp)
         if pkt_arp:
             self.parse_arp(pkt_arp, msg, pkt)
-            self.packet_count += 1
-            return
-
-        # Get the ICMP packet and parse it if it exists
-        pkt_icmp = pkt.get_protocol(icmp.icmp)
-        if pkt_icmp:
-            self.parse_icmp(pkt_icmp, msg, pkt)
             self.packet_count += 1
             return
 
@@ -110,31 +103,3 @@ class monitor(app_manager.RyuApp):
         p.add_protocol(a)
         p.serialize()
         print("%s", p)
-
-    '''
-        Parses an ICMP packet and prints important information
-    '''
-    def parse_icmp(self, pkt_icmp, msg, pkt):
-        pkt_eth = pkt.get_protocol(ethernet.ethernet) # Get the ethernet packet
-
-        datapath = msg.datapath
-        address, port = msg.datapath.address # Get the switch address and port
-
-        pkt_ipv4 = pkt.get_protocol(ipv4.ipv4) # Get the ipv4 packet if it exists
-
-        # Print out important information
-        self.logger.info("--------------------------------------------")
-        self.logger.info("Packet ( %s) Received on Port(%s) Eth PING", self.packet_count, msg.match['in_port'])
-        self.logger.info("\tPING")
-        self.logger.info("\tIPV4")
-        self.logger.info("\t\tCheck Sum: %s", pkt_ipv4.csum)
-        self.logger.info("\t\tFrom IP: %s", pkt_ipv4.src)
-        self.logger.info("\t\tTo   IP: %s", pkt_ipv4.dst)
-        self.logger.info("\t\tLength: %s", pkt_ipv4.total_length)
-        self.logger.info("\tNot IPV6")
-        self.logger.info("\tETH")
-        self.logger.info("\t\tFrom MAC: %s", pkt_eth.src)
-        self.logger.info("\t\tTo   MAC: %s", pkt_eth.dst)
-        self.logger.info("\tController Switch (OF)")
-        self.logger.info("\t\tAddress, Port: ('%s', %s)", address, port)
-
