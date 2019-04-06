166b84ca08c9f7a599c8f39e5e8b4b8eb6e2ddff
Nate <nategreat13@hotmail.com>
2019-04-06 16:59:44 -0600

    s

--- temp.py
+++ temp.py
@@ -206,15 +206,15 @@ class monitor(app_manager.RyuApp):
         Much of this logic was found by looking at the simple_switch_13.py file.
     '''
     def add_flow(self, datapath, priority, match, actions):
-    ofproto = datapath.ofproto
-    parser = datapath.ofproto_parser
-    
-    inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
-    mod = parser.OFPFlowMod(datapath=datapath, priority=priority, match=match, instructions=inst)
-    self.logger.info("--------------------")
-    self.logger.info("Adding Flow: %s",mod)
-    self.logger.info("--------------------")
-    datapath.send_msg(mod)
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
+        mod = parser.OFPFlowMod(datapath=datapath, priority=priority, match=match, instructions=inst)
+        self.logger.info("--------------------")
+        self.logger.info("Adding Flow: %s",mod)
+        self.logger.info("--------------------")
+        datapath.send_msg(mod)
 
 
 
