2725990c4f9b4adc682d839cb64c96a6d40ea5e6
Nate <nategreat13@hotmail.com>
2019-04-08 10:24:34 -0600

    s

--- u0773016.py
+++ u0773016.py
@@ -71,8 +71,6 @@ class monitor(app_manager.RyuApp):
         # Keep track of which back end server to assign the host to
         self.next_server_address_index = 0
 
-        self.canReceive = 1
-
 
     '''
         Handles packet in events
@@ -93,9 +91,6 @@ class monitor(app_manager.RyuApp):
         # Get the arp packet and parse it if it exists
         pkt_arp = pkt.get_protocol(arp.arp)
 
-        if self.canReceive == 0:
-            return
-
         # Only do something if we receive an ARP message
         if pkt_arp:
             self.parse_arp(pkt_arp, msg, pkt)
@@ -109,8 +104,6 @@ class monitor(app_manager.RyuApp):
         self.logger.info("--------------------")
         self.logger.info("Packet in: %s", pkt_arp)
         self.logger.info("--------------------")
-
-        self.canReceive = 0
 
         # Get the ethernet part of the packet
         eth = pkt.get_protocol(ethernet.ethernet)  # Get the ethernet packet
@@ -128,27 +121,6 @@ class monitor(app_manager.RyuApp):
         # an arp request back to the back end to update it's ARP table
         for i in range(len(self.front_end_macs_served)):
             if dst == self.front_end_macs_served[i]:
-                # Create the eth and arp packets to send to the back_end
-                # host and combine them into one packet
-                eth_pkt = ethernet.ethernet(dst=dst_mac, src=pkt_arp.src_mac, ethertype=ether.ETH_TYPE_ARP)
-                arp_pkt = arp.arp(hwtype=pkt_arp.hwtype, proto=pkt_arp.proto, hlen=pkt_arp.hlen, plen=pkt_arp.plen,
-                                  opcode=pkt_arp.opcode, src_mac=pkt_arp.src_mac, src_ip=pkt_arp.src_ip,
-                                  dst_mac=dst_mac, dst_ip=dst_ip)
-                p = packet.Packet()
-                p.add_protocol(eth_pkt)
-                p.add_protocol(arp_pkt)
-                p.serialize()
-
-                # Send the packet to the back_end server to update their
-                # arp table to point to the requesting host
-                data = p.data
-                actions = [parser.OFPActionOutput(port=back_end_port)]
-                out = parser.OFPPacketOut(datapath=datapath,
-                                          buffer_id=ofproto.OFP_NO_BUFFER,
-                                          in_port=ofproto.OFPP_CONTROLLER,
-                                          actions=actions,
-                                          data=data)
-                datapath.send_msg(out)
                 return
 
         # If the destination is not the virtual IP address, then don't do anything
@@ -204,6 +176,28 @@ class monitor(app_manager.RyuApp):
         actions = [parser.OFPActionOutput(port=in_port)]
         out = parser.OFPPacketOut(datapath=datapath, buffer_id=ofproto.OFP_NO_BUFFER,
                                   in_port=ofproto.OFPP_CONTROLLER, actions=actions, data=data)
+        datapath.send_msg(out)
+
+        # Create the eth and arp packets to send to the back_end
+        # host and combine them into one packet
+        eth_pkt = ethernet.ethernet(dst=dst_mac, src=pkt_arp.src_mac, ethertype=ether.ETH_TYPE_ARP)
+        arp_pkt = arp.arp(hwtype=pkt_arp.hwtype, proto=pkt_arp.proto, hlen=pkt_arp.hlen, plen=pkt_arp.plen,
+                          opcode=pkt_arp.opcode, src_mac=pkt_arp.src_mac, src_ip=pkt_arp.src_ip,
+                          dst_mac=dst_mac, dst_ip=dst_ip)
+        p = packet.Packet()
+        p.add_protocol(eth_pkt)
+        p.add_protocol(arp_pkt)
+        p.serialize()
+
+        # Send the packet to the back_end server to update their
+        # arp table to point to the requesting host
+        data = p.data
+        actions = [parser.OFPActionOutput(port=back_end_port)]
+        out = parser.OFPPacketOut(datapath=datapath,
+                                  buffer_id=ofproto.OFP_NO_BUFFER,
+                                  in_port=ofproto.OFPP_CONTROLLER,
+                                  actions=actions,
+                                  data=data)
         datapath.send_msg(out)
 
     '''
